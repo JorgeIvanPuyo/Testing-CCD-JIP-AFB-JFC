a619186dc56ed0e9b3d655a8dde50c1b23e92bf3
Jorge Ivan Puyo <ji.puyo@uniandes.edu.co>
2023-11-11 21:19:38 -0500

    Implementacion escenario 2 Ghost Kraken terminada

--- Kraken/features/web/pages/dashboard_page.js
+++ Kraken/features/web/pages/dashboard_page.js
@@ -8,6 +8,11 @@ class Dashboard {
         return await element.click();
     }
 
+    async clickPostsIcon() {
+        let element = await this.driver.$('[data-test-nav="new-story"]');
+        return await element.click();
+    }
+
     async getCurrentUrl() {
         return await this.driver.getUrl();
     }
