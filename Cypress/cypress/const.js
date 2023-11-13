6c3472296e378bc4d17db42573e138d644ce95ed
Juan F Copete <jf.copete@uniandes.edu.co>
2023-11-12 22:48:24 -0500

    Fake Credentials Leaked, Correction

--- Cypress/cypress/const.js
+++ Cypress/cypress/const.js
@@ -1,3 +1,3 @@
-export const USER = "jf.copete@uniandes.edu.co";
-export const PASSWORD = "F3rchoco7833947.";
+export const USER = "*";
+export const PASSWORD = "*";
 export const APP_PAGE = "http://localhost:2368";
