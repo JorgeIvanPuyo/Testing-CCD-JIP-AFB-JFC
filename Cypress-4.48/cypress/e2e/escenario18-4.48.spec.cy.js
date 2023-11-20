65e21fe32f50ed7873ab75758cc49203b6e9199d
Juan F Copete <jf.copete@uniandes.edu.co>
2023-11-19 19:16:25 -0500

    SS En todos los escenarios

--- Cypress-4.48/cypress/e2e/escenario18-4.48.spec.cy.js
+++ Cypress-4.48/cypress/e2e/escenario18-4.48.spec.cy.js
@@ -8,6 +8,10 @@ describe("Gestión de Miembros en la Comunidad", function () {
     it("Intentar crear un nuevo miembro y luego abandonar el proceso", function () {
       // Given Un usuario con credenciales válidas
       cy.visit(`${APP_PAGE}/ghost/#/signin`);
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
       cy.wait(1000);
       
       const signinPage = new SigninPage(cy);
@@ -16,42 +20,81 @@ describe("Gestión de Miembros en la Comunidad", function () {
       // When El usuario inicia sesión con 'user' y 'password' válidos
       signinPage.loginValidUser(USER, PASSWORD);
       cy.url().should("contain", "/dashboard");
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario navega a la página de 'Members'
       cy.wait(100);
-
       membersPage.navigateToMembers();
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario hace clic en 'New Member'
       membersPage.getNewMemberButton().click();
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario completa el campo 'nombre' con un nombre válido
       const memberName = faker.person.fullName();
       membersPage.getMemberNameInput().type(memberName,{force: true});
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
       
       const memberEmail = faker.internet.email();
       membersPage.getMemberEmailInput().type(memberEmail);
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario completa el campo 'labels' con etiquetas relevantes
       const labels = faker.lorem.word();
       membersPage.getLabelMember().type(`${labels}{enter}`);
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario completa el campo 'note' con una nota relevante
       const note = faker.lorem.sentence();
       membersPage.getMemberNoteTextarea().type(note);
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // And El usuario intenta salir de la página
       membersPage.navigateToMembers();
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // Then Se muestra un mensaje de confirmación de abandono
       // Aquí se asume que hay un mecanismo para detectar el intento de navegación y mostrar un mensaje de confirmación
   
       // When El usuario hace clic en 'Leave'
       membersPage.getLeaveButton().click();
+      cy.screenshot({
+        capture: "viewport",
+        scale: true,
+      });
   
       // Then El nuevo miembro no se crea y no aparece en la lista de miembros
       membersPage.memberDoesNotExist(memberEmail).then(result => {
         expect(result).to.be.true;
+        cy.screenshot({
+          capture: "viewport",
+          scale: true,
+        });
       });
     });
 });
--- Cypress-4.48/cypress/e2e/escenario20-4.48.spec.cy.js
+++ Cypress-4.48/cypress/e2e/escenario20-4.48.spec.cy.js
@@ -10,6 +10,10 @@ describe("Gestión de Tags en la Plataforma", function () {
   it("Crear un nuevo tag, asignarlo a un post y luego verificar la asignación y desasignación", function () {
     // Given Un usuario con credenciales válidas
     cy.visit(`${APP_PAGE}/ghost/#/signin`);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
     const signinPage = new SigninPage(cy);
     const modifyPostPage = new ModifyPostPage(cy);
     const tagPage = new TagPage(cy);
@@ -17,71 +21,150 @@ describe("Gestión de Tags en la Plataforma", function () {
     // When El usuario inicia sesión con 'user' y 'password' válidos
     signinPage.loginValidUser(USER, PASSWORD);
     cy.url().should("contain", "/dashboard");
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en el botón 'Tags' en el panel de navegación
     cy.wait(100);
     tagPage.navigateToTags();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'New Tag'
     tagPage.getNewTagButton().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario agrega un 'nombre' para el tag
     const tagName = faker.lorem.word();
     tagPage.getTagNameInput().type(tagName);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario agrega un 'color' para el tag
     const tagColor = faker.internet.color().substring(1); // Remover '#'
     tagPage.getTagAccentColorInput().type(tagColor);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario agrega un 'slug' para el tag
     const tagSlug = faker.lorem.slug();
     tagPage.getTagSlugInput().type(tagSlug);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // Then Se valida que la vista previa del 'slug' cambió
     tagPage.getTagSlugPreview().should('contain', tagSlug);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario agrega una 'descripción' para el tag
     const tagDescription = faker.lorem.sentence();
     tagPage.getTagDescriptionTextarea().type(tagDescription);
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'Save'
     tagPage.getSaveButton().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'Tags'
     tagPage.navigateToTags();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // Then Se valida que se haya creado un tag nuevo
     cy.contains(tagName).should('exist');
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'Posts'
     modifyPostPage.navigateToPosts();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario selecciona un post en estado 'Draft'
     modifyPostPage.getPostDraftToModify().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en el menú colapsable 'Settings'
     modifyPostPage.getSettingsButton().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'Tags'
     modifyPostPage.getTagInputDiv().click();
     modifyPostPage.getFirstEmberPowerSelectOption().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // Then Se valida que el tag recién creado esté disponible
     cy.contains(tagName).should('exist');
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en el nuevo tag
     cy.contains(tagName).click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en el menú colapsable 'Settings'
     modifyPostPage.getSettingsButton().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en 'Update'
     modifyPostPage.navigateToPosts();
     modifyPostPage.getLeaveButton().click();
     modifyPostPage.getPostDraftToModify().click();
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
 
     // And El usuario hace clic en el menú colapsable 'Settings' nuevamente
     modifyPostPage.getSettingsButton().click();
-
+    cy.screenshot({
+      capture: "viewport",
+      scale: true,
+    });
   });
 });
