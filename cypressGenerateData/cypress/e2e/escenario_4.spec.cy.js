ecf0addaceb3f968f741758beba1452326dbac70
Cristhian <ccdelgadop@gmail.com>
2023-11-26 17:30:06 -0500

    fix tests

--- cypressGenerateData/cypress/e2e/escenario_4.spec.cy.js
+++ cypressGenerateData/cypress/e2e/escenario_4.spec.cy.js
@@ -34,10 +34,14 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       posts.fillPostDescription(description);
       // When: el usuario haga click sobre publicar pero en el ultimo paso nolo hace
       posts.clickPublishButton();
+      cy.wait(1000);
       posts.clickContinueAndReviewButton();
+      cy.wait(1000);
       posts.goToEdit();
+      cy.wait(1000);
       // Then: el usuario podra ver el post sin publicar
       posts.goToPostsList();
+      cy.wait(1000);
 
       // Given: a list of posts
       const postList = new PostsListPage(cy);
@@ -49,13 +53,16 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
       });
+      cy.wait(1000);
 
       // Given: Dada la lista de post
       const postList2 = new PostsListPage(cy);
       // When: el usuario busca un post por titulo
       const postToEdit = postList2.getPostByTitle(title);
       // Then: el usuario podra editar el post haciendo click sobre el icono de editar
+      cy.wait(500);
       const postEditPage = postList2.goToEditPostUnpublish(postToEdit);
+      cy.wait(500);
 
       // When: el usuario ingrese el nuevo titulo y descripción
       const newTitle = faker.person.jobTitle();
@@ -64,13 +71,16 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       postEditPage.fillPostDescription(newDescription);
       // Then: el usuario regresa al listado de post y vera el post con el nuevo titulo, descripción y aun como borrador
       postEditPage.goToPostsList();
+      cy.wait(500);
       const postList3 = new PostsListPage(cy);
       const postEdited = postList3.getPostByTitle(title + newTitle);
       const statusEdited = postList3.getStatusPost(postEdited);
+      cy.wait(500);
       statusEdited.should(($status) => {
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
       });
+      cy.wait(500);
     });
   });
 
@@ -99,8 +109,11 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       posts.fillPostDescription(description);
       // When: el usuario haga click sobre publicar pero en el ultimo paso nolo hace
       posts.clickPublishButton();
+      cy.wait(1000);
       posts.clickContinueAndReviewButton();
+      cy.wait(1000);
       posts.goToEdit();
+      cy.wait(1000);
       // Then: el usuario podra ver el post sin publicar
       posts.goToPostsList();
 
@@ -114,6 +127,7 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
       });
+      cy.wait(500);
 
       // Given: Dada la lista de post
       const postList2 = new PostsListPage(cy);
@@ -125,17 +139,21 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       // When: el usuario ingrese el nuevo titulo y descripción
       const newTitle = faker.person.jobTitle();
       const newDescription = faker.lorem.paragraph();
+      cy.wait(500);
       postEditPage.fillPostTitle(newTitle);
       postEditPage.fillPostDescription(newDescription);
       // Then: el usuario regresa al listado de post y vera el post con el nuevo titulo, descripción y aun como borrador
       postEditPage.goToPostsList();
+      cy.wait(500);
       const postList3 = new PostsListPage(cy);
       const postEdited = postList3.getPostByTitle(title + newTitle);
       const statusEdited = postList3.getStatusPost(postEdited);
+      cy.wait(500);
       statusEdited.should(($status) => {
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
       });
+      cy.wait(500);
     });
   });
 
@@ -156,6 +174,7 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       const posts = new CreatePostPage(cy);
       // Then: el usuario estara en el formulario de crear in post
       posts.entryToPostListByIcon();
+      cy.wait(500);
 
       // Given: los campos generados de forma apriori
       const { title, description } = getAprioriPostData(0);
@@ -164,8 +183,11 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       posts.fillPostDescription(description);
       // When: el usuario haga click sobre publicar pero en el ultimo paso nolo hace
       posts.clickPublishButton();
+      cy.wait(1000);
       posts.clickContinueAndReviewButton();
+      cy.wait(1000);
       posts.goToEdit();
+      cy.wait(1000);
       // Then: el usuario podra ver el post sin publicar
       posts.goToPostsList();
 
@@ -175,10 +197,12 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       const post = postList.getPostByTitle(title);
       const status = postList.getStatusPost(post);
       // Then: el usuario podra validar que esta como borrador es decir no se ha publicado
+      cy.wait(500);
       status.should(($status) => {
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
       });
+      cy.wait(500);
 
       // Given: Dada la lista de post
       const postList2 = new PostsListPage(cy);
@@ -186,6 +210,7 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       const postToEdit = postList2.getPostByTitle(title);
       // Then: el usuario podra editar el post haciendo click sobre el icono de editar
       const postEditPage = postList2.goToEditPostUnpublish(postToEdit);
+      cy.wait(500);
 
       // When: el usuario ingrese el nuevo titulo y descripción
       const newTitle = faker.person.jobTitle();
@@ -194,9 +219,11 @@ describe("Como usuario quiero editar un post que no este publicado para actualiz
       postEditPage.fillPostDescription(newDescription);
       // Then: el usuario regresa al listado de post y vera el post con el nuevo titulo, descripción y aun como borrador
       postEditPage.goToPostsList();
+      cy.wait(500);
       const postList3 = new PostsListPage(cy);
       const postEdited = postList3.getPostByTitle(title + newTitle);
       const statusEdited = postList3.getStatusPost(postEdited);
+      cy.wait(500);
       statusEdited.should(($status) => {
         if (!$status.length) return;
         expect($status[0]).to.contain.text("Draft");
